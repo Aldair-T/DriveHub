b1e6609097ea7978f05a68425618ca0b7cfa3573
Aldair-T <atorresq@fi.uba.ar>
2021-07-17 23:09:36 -0300

    Codigo para crear archivos

--- CrearArchivos.py
+++ CrearArchivos.py
@@ -2,12 +2,14 @@ import os
 
 
 def tipos_archivos() -> None:
+    # Tipos de archivos que se pueden crear (Pueden faltar algunos)
     print("1) Crear una carpeta\n"
           "2) Crear un archivo .txt\n"
           "3) Crear un archivo .csv\n")
 
 
 def crear_carpeta() -> None:
+    # Crea carpetas, falta que naveguen en el escritorio
     nombre_carpeta = input("Ingrese el nombre de la carpeta: ")
     if os.path.isdir(nombre_carpeta):
         print("la carpeta existe")
@@ -16,6 +18,7 @@ def crear_carpeta() -> None:
 
 
 def crear_archivo_txt() -> None:
+    # Crea archivos .txt falta la navegacion
     nombre_archivo = input("Ingrese el nombre del nuevo archivo: ")
     nombre_archivo += ".txt"
     nuevo_archivo = open(nombre_archivo, "w")
@@ -23,6 +26,7 @@ def crear_archivo_txt() -> None:
 
 
 def crear_archivo_csv() -> None:
+    # Crea archivos .csv, falta la navegacion
     nombre_archivo = input("Ingres el nombre del nuevo archivo: ")
     nombre_archivo += ".csv"
     nuevo_archivo = open(nombre_archivo, "w")
@@ -30,6 +34,7 @@ def crear_archivo_csv() -> None:
 
 
 def creacion_archivos() -> None:
+    # Es el menu para ver que archivo va a crear
     tipos_archivos()
     opcion = input("Que quieres crear: ")
     while not opcion.isnumeric() or int(opcion) < 1 or int(opcion) > 3:
