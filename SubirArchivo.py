91aea83307ca80f0aa87754fd7b7a6b8a5750d6f
Aldair-T <atorresq@fi.uba.ar>
2021-07-21 19:31:32 -0300

    Añado la funcion para subir archivos

--- SubirArchivo.py
+++ SubirArchivo.py
@@ -1,15 +1,22 @@
+import os
 from service_drive import obtener_servicio
 from googleapiclient.http import MediaFileUpload
 
-'''id_carpeta = ""
-nombre_archivos = [""]
-tipo_archivos = [".jpg (etc)"]
 
-for nombre_archivo, tipo_archivo in zip(nombre_archivos, tipo_archivos):
-    file_metadata = {'name': "nombre", 'carpeta': [id_carpeta]}
-    media = MediaFileUpload('files/photo.jpg', mimetype = 'image/jpeg')
-    file = obtener_servicio().files().create(body = file_metadata,
-                                             media_body = media,
-                                             fields = 'id').execute()
-    print('File ID: %s' % file.get('id'))
-'''
+def subiendo_archivo(archivo: str) -> None:
+    archivos_metadata = {'name': archivo}
+    media = MediaFileUpload(archivo)
+    obtener_servicio().files().create(body = archivos_metadata,
+                                      media_body = media,
+                                      fields = 'id').execute()
+    print("Archivo subido con éxito")
+
+
+def subir_archivos() -> None:
+    nombre_archivo = input("Ingrese el nombre del archivo: ")
+    tipo_archivo = input("Ingrese el tipo de archivo: ")
+    archivo = nombre_archivo + tipo_archivo
+    if os.path.exists(archivo):
+        subiendo_archivo(archivo)
+    else:
+        print("Ese archivo no existe")
