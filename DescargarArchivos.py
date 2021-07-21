dd51e7d5cf3c6fe5e5905a9b4274c75928d62da0
Aldair-T <atorresq@fi.uba.ar>
2021-07-21 20:32:07 -0300

    Funcion para descargar archivos de drive

--- DescargarArchivos.py
+++ DescargarArchivos.py
@@ -3,17 +3,26 @@ import io
 from googleapiclient.http import MediaIoBaseDownload
 from service_drive import obtener_servicio
 
-# Es una maqueta, hay errores q no puedo resolver, no entiendo el metodo
 
-'''servicio = obtener_servicio()
-file_id = ['0BwwA4oUTeiV1UVNwOHItT0xfa2M']
-file_name = ['hola']
-for file_id, file_name in zip(file_id,file_name):
-    request = servicio.files().get_media(fileId = file_id)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        print("Download progress {0}" .format(status.progress() * 100))
-'''
+def descargar_archivos() -> None:
+    # file_ids = ['1VVXGTbAJ649n2u8k7sW3M9YDggnpbl84']
+    ids_archivo = input("Ingrese el id de su archivo: ")
+    ids_archivo = ids_archivo.split(" ")
+    nombre = input("Ingrese el nombre para el archivo: ")
+    tipo_archivo = input("Ingrese el tipo de archivo (ejem : .txt) : ")
+    nombre_archivos = nombre + tipo_archivo
+    nombre_archivos = nombre_archivos.split(" ")
+    # file_names = ["lee.jpeg"]
+    for id_archivo, nombre_archivo in zip(ids_archivo, nombre_archivos):
+        respuesta = obtener_servicio().files().get_media(fileId = id_archivo)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fd = fh, request = respuesta)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+            done = True
+        fh.seek(0)
+        with open(os.path.join("./Descargas", nombre_archivo), "wb") as archivos:
+            archivos.write(fh.read())
+            archivos.close()
