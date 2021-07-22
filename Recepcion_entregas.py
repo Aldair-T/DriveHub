075c23ceba36224c9370a9261527ac6b90f59f48
Lucas-Jun <75916652+Lucas-Jun@users.noreply.github.com>
2021-07-21 21:11:29 -0300

    Update Recepcion_entregas.py

--- Recepcion_entregas.py
+++ Recepcion_entregas.py
@@ -3,6 +3,7 @@ from service_gmail import obtener_servicio
 import os
 from email.mime.text import MIMEText
 import base64
+from email import errors
 
 def enviar_mensaje():
     gmail_de = "ljun@fi.uba.ar"
@@ -14,9 +15,18 @@ def enviar_mensaje():
     message['to'] = gmail_para
     message['from'] = gmail_de
     message['subject'] = asunto
-    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+    raw = base64.urlsafe_b64encode(message.as_bytes())
+    raw = raw.decode()
+    body= {'raw': raw}
 
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message)
+                .execute())
+        print ("Mensaje enviado")
+    except errors.MessageError as error:
+        print ('An error occurred: %s' % error)
 
 def main():
     enviar_mensaje()
-    
+  
+main()
