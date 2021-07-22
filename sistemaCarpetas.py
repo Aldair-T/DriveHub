1379bd64462f93c9fb1e54d12b4109b18915d8a2
tobiasgrati <tgrati@fi.uba.ar>
2021-07-22 18:39:40 -0300

    Lectura de mails y drive

--- sistemaCarpetas.py
+++ sistemaCarpetas.py
@@ -1,17 +1,31 @@
-from service_drive import obtener_servicio
+from service_drive import obtener_servicio as SERVICE_DRIVE
+from service_gmail import obtener_servicio as SERVICE_GMAIL
 
+carpeta_TP2_ID = "1356xXW9laVirDDJv1dEKgkAnw5zT1Rc3"
 
+def carpetas_encontradas():
+    carpetas_examenes_encontradas = list()
+    arpetas_alumnos_encontradas = list()
+    arpetas_profesores_encontradas = list()
 
-nombre = input("Ingrese nombre de carpeta")
+    carpetas = SERVICE_DRIVE().files().list(q = f"mimeType = 'application/vnd.google-apps.folder' and parents in '{carpeta_TP2_ID}'").execute()
 
-def creacion_carpeta():
+    print(carpetas)
+
+    for carpeta in carpetas['files']:
+        print(carpeta['name'])
+    
+    
+
+def creacion_carpeta(nombre):
 
     file_metadata = {
         'name': nombre,
-        'mimeType': 'application/vnd.google-apps.folder'
+        'mimeType': 'application/vnd.google-apps.folder',
+        'parents' : [f'{carpeta_TP2_ID}']
     }
 
-    carpeta_examenes = obtener_servicio().files().create(body=file_metadata).execute()
+    carpeta_examenes = SERVICE_DRIVE().files().create(body=file_metadata).execute()
 
     carpeta_examenes_ID = carpeta_examenes.get('id')
 
@@ -21,7 +35,7 @@ def creacion_carpeta():
         'parents': [carpeta_examenes_ID]
     }
 
-    carpeta_profesores = obtener_servicio().files().create(body=carpeta_profesores_metadata).execute()
+    carpeta_profesores = SERVICE_DRIVE().files().create(body=carpeta_profesores_metadata).execute()
 
     carpeta_profesores_ID = carpeta_profesores.get('id')
 
@@ -31,9 +45,38 @@ def creacion_carpeta():
         'parents': [carpeta_profesores_ID]      
     }
     
-    obtener_servicio().files().create(body=carpeta_alumnos_metadata).execute()
-                                    
-                                            
+    SERVICE_DRIVE().files().create(body=carpeta_alumnos_metadata).execute()
 
-creacion_carpeta()
+
+
+def leer_mail():
+
+    resultados = SERVICE_GMAIL().users().messages().list(userId='me').execute()
+
+    id_mails = []
+    contador = 0
+
+    for mail in resultados:
+        mail_ID = (resultados['messages'][contador]['id'])
+        id_mails.append(mail_ID)
+        contador += 1
+
+    for id_mail in id_mails:
+
+        mail = SERVICE_GMAIL().users().messages().get(userId='me', id=id_mail, format='full').execute()
+
+        for value in mail['payload']['headers']:
+            if value['name'] == 'Subject':
+                asunto = (value['value']).split()
+                if asunto[0] == "nombre_examen":
+                    nombre_examen = asunto[1]
+                    creacion_carpeta(nombre_examen)
+
+
+
+carpetas_encontradas()
+
+leer_mail()
+
+
 
