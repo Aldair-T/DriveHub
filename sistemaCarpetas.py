0adcaf7d9365d20ef691ea05e6c726550a5f0746
tobiasgrati <tgrati@fi.uba.ar>
2021-07-22 22:07:04 -0300

    Creacion de carpetas por docente/alumno

--- sistemaCarpetas.py
+++ sistemaCarpetas.py
@@ -1,9 +1,10 @@
 from service_drive import obtener_servicio as SERVICE_DRIVE
 from service_gmail import obtener_servicio as SERVICE_GMAIL
+import csv
 
 carpeta_TP2_ID = "1356xXW9laVirDDJv1dEKgkAnw5zT1Rc3"
 
-def carpetas_encontradas():
+def carpetas_encontradas() -> None:
 
     carpetas_en_drive = list()
 
@@ -13,48 +14,73 @@ def carpetas_encontradas():
         carpetas_en_drive.append(carpeta['name'])
     
     leer_mail(carpetas_en_drive)
-    
-    
 
-def creacion_carpeta(nombre):
+def creacion_carpeta_alumnos(carpeta_docente_ID: str,docente: str) -> None:
+    lista_alumnos = []
+
+    with open("docente-alumno.csv", mode= 'r',newline= '', encoding= "UTF-8") as archivo_csv:
+        csv_reader = csv.reader(archivo_csv,delimiter=',')
+        for linea in csv_reader:
+            if linea[0] == docente:
+                lista_alumnos.append(linea[1])
+
+
+    for alumno in lista_alumnos:
+        carpeta_alumnos_metadata = {        
+            'name': alumno,
+            'mimeType': 'application/vnd.google-apps.folder',
+            'parents': [carpeta_docente_ID]      
+        }
+        
+        SERVICE_DRIVE().files().create(body=carpeta_alumnos_metadata).execute()
+
+def creacion_carpeta_docentes(carpeta_examenes_ID: str) -> None:
+
+    lista_docentes = []
+
+
+    with open("docentes.csv", mode= 'r',newline= '', encoding= "UTF-8") as archivo_csv:
+        csv_reader = csv.reader(archivo_csv,delimiter=',')
+        for linea in csv_reader:
+            lista_docentes.append(linea[0])
+
+ 
+
+    for docente in lista_docentes:    
+        carpeta_docentes_metadata = {
+            'name': docente,
+            'mimeType': 'application/vnd.google-apps.folder',
+            'parents': [carpeta_examenes_ID]
+        }
+        carpeta_docentes = SERVICE_DRIVE().files().create(body=carpeta_docentes_metadata).execute()
+
+        carpeta_docente_ID = carpeta_docentes.get('id')
+
+        creacion_carpeta_alumnos(carpeta_docente_ID,docente)
+
+
+def creacion_carpeta(nombre: str)-> None:
 
     file_metadata = {
         'name': nombre,
         'mimeType': 'application/vnd.google-apps.folder',
         'parents' : [f'{carpeta_TP2_ID}']
     }
-
+    
     carpeta_examenes = SERVICE_DRIVE().files().create(body=file_metadata).execute()
 
     carpeta_examenes_ID = carpeta_examenes.get('id')
 
-    carpeta_profesores_metadata = {
-        'name': "profesores",
-        'mimeType': 'application/vnd.google-apps.folder',
-        'parents': [carpeta_examenes_ID]
-    }
-
-    carpeta_profesores = SERVICE_DRIVE().files().create(body=carpeta_profesores_metadata).execute()
-
-    carpeta_profesores_ID = carpeta_profesores.get('id')
-
-    carpeta_alumnos_metadata = {        
-        'name': "alumnos",
-        'mimeType': 'application/vnd.google-apps.folder',
-        'parents': [carpeta_profesores_ID]      
-    }
-    
-    SERVICE_DRIVE().files().create(body=carpeta_alumnos_metadata).execute()
+    creacion_carpeta_docentes(carpeta_examenes_ID)
 
 
-
-def leer_mail(carpetas_en_drive):
+def leer_mail(carpetas_en_drive: list) -> None:
 
     resultados = SERVICE_GMAIL().users().messages().list(userId='me').execute()
 
     id_mails = []
     contador = 0
-
+    
     for mail in resultados:
         mail_ID = (resultados['messages'][contador]['id'])
         id_mails.append(mail_ID)
