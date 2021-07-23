9f0e58c4492a1e66edad6687d8c83c01cad5bdcd
Aldair-T <atorresq@fi.uba.ar>
2021-07-22 22:34:06 -0300

    Maqueta para sincronizar

--- SincronizarArchivos.py
+++ SincronizarArchivos.py
@@ -1,10 +1,13 @@
 import os
+import io
 import pathlib
 import time
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+
 from service_drive import obtener_servicio
 
 
-def modificacion_tiempo(archivo) -> str:
+def obtener_tiempo(archivo) -> str:
     tiempo = os.path.getmtime(archivo)
     anio, mes, dia, hora, minuto, segundo = time.localtime(tiempo)[: -3]
     lista_fecha = [anio, mes, dia, hora, minuto, segundo]
@@ -23,7 +26,7 @@ def archivos_local() -> dict:
     carpetas = pathlib.Path('/Users/aldai/PycharmProjects/pythonProject')
     archivos_locales = {}
     for archivo in carpetas.iterdir():
-        tiempo = modificacion_tiempo(archivo)
+        tiempo = obtener_tiempo(archivo)
         archivos_locales[archivo.name] = tiempo
     return archivos_locales
 
@@ -63,17 +66,54 @@ def archivos_drive() -> dict:
         tiempo = file.get('modifiedTime')
         tiempo = modificar_estructura(tiempo)
         nombre = file.get('name')
-        archivos_remotos[nombre] = tiempo
+        id_archivo = file.get('id')
+        archivos_remotos[nombre] = [tiempo, id_archivo]
     return archivos_remotos
 
 
+def subir_modific_drive(nombre_archivo: str, id_archivo: str) -> None:
+    obtener_servicio().files().delete(fileId = id_archivo)
+    archivos_metadata = {'name': nombre_archivo}
+    media = MediaFileUpload(nombre_archivo)
+    obtener_servicio().files().create(body = archivos_metadata,
+                                      media_body = media,
+                                      fields = 'id').execute()
+    print("Se modificaron los archivos")
 
 
-"""
-archivos_locales = {aldair.txt: 39399393, alda.jpg: 832746}
-archivos_remotos = {alda.txt: 923847y2, hola: 4338, aldair.txt: 8376473}
-for x, y in archivos_locales.items():
-    for z, k in archivos_remotos.items():
-        if x == z:
-            
-"""
+def subir_modific_local(nombre_archivo: str, ids_archivo: str) -> None:
+    nombre_archivos = nombre_archivo.split(" ")
+    ids_archivos = ids_archivo.split(" ")
+    for ids, nombre in zip(ids_archivos, nombre_archivos):
+        respuesta = obtener_servicio().files().get_media(fileId = ids_archivos)
+        fh = io.BytesIO()
+        descarga = MediaIoBaseDownload(fd = fh, request = respuesta)
+        salir = False
+        while salir is False:
+            estado, salir = descarga.next_chunk()
+            estado = 0
+            salir = True
+        fh.seek(0)
+        with open(os.path.join("./Descargas_Drive", nombre_archivo), "wb") as archivos:
+            archivos.write(fh.read())
+            print("Se modifico los archivos")
+            archivos.close()
+        os.remove(nombre_archivo)
+
+
+def sincronizacion() -> None:
+    dict_local = archivos_local()
+    print(dict_local)
+    dict_drive = archivos_drive()
+    print(dict_drive)
+
+    for nombres_local, modificacion_local in dict_local.items():
+        for nombres_drive, modificacion_drive in dict_drive.items():
+            if nombres_local == nombres_drive:
+                print(modificacion_drive[0])
+                if modificacion_local > modificacion_drive[0]:
+                    print("badyb")
+                    subir_modific_drive(nombres_local, modificacion_drive[1])
+                elif modificacion_local < modificacion_drive[0]:
+                    print("uadb")
+                    subir_modific_local(nombres_local, modificacion_drive[1])
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ListadoArchivos import listar_archivos
 from CrearArchivos import creacion_archivos
 from DescargarArchivos import crear_carpeta_descargas
 from SubirArchivo import subir_archivos
-from SincronizarArchivos import timepo
+from SincronizarArchivos import sincronizacion
 
 def menu() -> None:
     print("1) Listar archivos\n"
@@ -37,7 +37,7 @@ def main() -> None:
         if opcion == 4:
             crear_carpeta_descargas()
         if opcion == 5:
-            timepo()
+            sincronizacion()
         if opcion == 6:
             pass
         if opcion == 7:
