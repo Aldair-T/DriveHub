0aba3d8e68e83d506b5e93cffc64242d2c209058
Lucas-Jun <75916652+Lucas-Jun@users.noreply.github.com>
2021-07-25 22:04:17 -0300

    Update Recepcion_entregas.py

--- Recepcion_entregas.py
+++ Recepcion_entregas.py
@@ -1,11 +1,23 @@
-from pathlib import Path
-from service_gmail import obtener_servicio as SERVICE_GMAIL
-import os
-from email.mime.text import MIMEText
 import base64
+from email.mime.text import MIMEText
 from email import errors
+import os
+
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+ARCHIVO_SECRET_CLIENT = 'client_secret.json'
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.send'
+]
 
 def enviar_mensaje():
+    credencial = None
+
+    if os.path.exists('token.json'):
+        with open('token.json', 'r'):
+            credencial = Credentials.from_authorized_user_file('token.json', SCOPES)
+
     gmail_de = "ljun@fi.uba.ar"
     gmail_para = "ljun@fi.uba.ar"
     asunto ="La entrega fue..."
@@ -18,9 +30,9 @@ def enviar_mensaje():
     raw = base64.urlsafe_b64encode(message.as_bytes())
     raw = raw.decode()
     body= {'raw': raw}
-
+    serv= build('gmail', 'v1', credentials=credencial)
     try:
-        message = (SERVICE_GMAIL.users().messages().send(userId='me', body=body).execute())
+        message = (serv.users().messages().send(userId='me', body=body).execute())
         print ("Mensaje enviado")
     except errors.MessageError as error:
         print ('An error occurred: %s' % error)
