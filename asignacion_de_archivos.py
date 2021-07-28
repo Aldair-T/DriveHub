a0c054a38d34215d5e523f0b025e97ef3323ea86
JoaquinTamborini <86444808+JoaquinTamborini@users.noreply.github.com>
2021-07-28 11:47:46 -0300

    Add files via upload

--- asignacion_de_archivos.py
+++ asignacion_de_archivos.py
@@ -2,25 +2,37 @@ from service_drive import obtener_servicio as SERVICE_DRIVE
 from service_gmail import obtener_servicio as SERVICE_GMAIL
 import csv
 
-def leer_mail(carpetas_en_drive: list) -> None:
-
+def importar_archivos(alumnos):
     resultados = SERVICE_GMAIL().users().messages().list(userId='me').execute()
-
-
     id_mails = []
     contador = 0
-    
     for mail in resultados['messages']:
         mail_ID = (resultados['messages'][contador]['id'])
-        id_mails.append(mail_ID)
         contador += 1
-
-    for id_mail in id_mails:
-
-        archivo = SERVICE_GMAIL().users().attachments().get(userId='me', messageId=id_mail, x__xgafv=None).execute()
-
+        
         for padron in mail['payload']['headers']:
-            #encajar padron con nombre y apellido
+            nombre = alumnos[padron]
+
+            archivo = SERVICE_GMAIL().users().attachments().get(userId='me', messageId=mail_ID, x__xgafv=None).execute()
+
+            #utilizar nombre para guardar archivo en carpeta
+        
+
+def padron_con_nombre():
+    with open("alumnos.csv", mode= 'r',newline= '', encoding= "UTF-8") as archivo_csv:
+        csv_reader = csv.reader(archivo_csv,delimiter=',')
+        for linea in csv_reader:
+            alumnos[linea[1]] = linea[0]
+    
+
+def main():
+    alumnos = {}
+    padron_con_nombre(alumnos)
+    importar_archivos(alumnos)
 
 
-leer_mail()
+    
+            
+
+
+
